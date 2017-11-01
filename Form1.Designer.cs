c075561735d1818dba0ea3acd8b5d1260f63a649
regameview <regameview@REGAMEVIEW->
2017-11-01 18:50:02 +0300

    add files

---
+++ Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.G = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.X = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Y = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Z = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.E = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.P = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.S = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.F = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.создатьGCodeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьКакToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllHeaders;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.G,
+            this.X,
+            this.Y,
+            this.Z,
+            this.E,
+            this.P,
+            this.S,
+            this.F});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.dataGridView1.Size = new System.Drawing.Size(680, 284);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.Visible = false;
+            // 
+            // G
+            // 
+            this.G.HeaderText = "Main";
+            this.G.Name = "G";
+            // 
+            // X
+            // 
+            this.X.HeaderText = "X";
+            this.X.Name = "X";
+            // 
+            // Y
+            // 
+            this.Y.HeaderText = "Y";
+            this.Y.Name = "Y";
+            // 
+            // Z
+            // 
+            this.Z.HeaderText = "Z";
+            this.Z.Name = "Z";
+            // 
+            // E
+            // 
+            this.E.HeaderText = "E";
+            this.E.Name = "E";
+            // 
+            // P
+            // 
+            this.P.HeaderText = "P";
+            this.P.Name = "P";
+            // 
+            // S
+            // 
+            this.S.HeaderText = "S";
+            this.S.Name = "S";
+            // 
+            // F
+            // 
+            this.F.HeaderText = "F";
+            this.F.Name = "F";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлыToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(680, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлыToolStripMenuItem
+            // 
+            this.файлыToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.создатьGCodeToolStripMenuItem,
+            this.открытьToolStripMenuItem,
+            this.сохранитьToolStripMenuItem,
+            this.сохранитьКакToolStripMenuItem});
+            this.файлыToolStripMenuItem.Name = "файлыToolStripMenuItem";
+            this.файлыToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.файлыToolStripMenuItem.Text = "Файлы";
+            // 
+            // создатьGCodeToolStripMenuItem
+            // 
+            this.создатьGCodeToolStripMenuItem.Name = "создатьGCodeToolStripMenuItem";
+            this.создатьGCodeToolStripMenuItem.Size = new System.Drawing.Size(192, 22);
+            this.создатьGCodeToolStripMenuItem.Text = "Создать GCode";
+            this.создатьGCodeToolStripMenuItem.Click += new System.EventHandler(this.создатьGCodeToolStripMenuItem_Click);
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(192, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть файл GCode";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(192, 22);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
+            // 
+            // сохранитьКакToolStripMenuItem
+            // 
+            this.сохранитьКакToolStripMenuItem.Name = "сохранитьКакToolStripMenuItem";
+            this.сохранитьКакToolStripMenuItem.Size = new System.Drawing.Size(192, 22);
+            this.сохранитьКакToolStripMenuItem.Text = "Сохранить как...";
+            this.сохранитьКакToolStripMenuItem.Click += new System.EventHandler(this.сохранитьКакToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 316);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Добавить строку";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(158, 326);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 351);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "GCode";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьКакToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem создатьGCodeToolStripMenuItem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn G;
+        private System.Windows.Forms.DataGridViewTextBoxColumn X;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Y;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Z;
+        private System.Windows.Forms.DataGridViewTextBoxColumn E;
+        private System.Windows.Forms.DataGridViewTextBoxColumn P;
+        private System.Windows.Forms.DataGridViewTextBoxColumn S;
+        private System.Windows.Forms.DataGridViewTextBoxColumn F;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        //Оптимизация: -1
+        //Качество кода == Оптимизации
+        //Время: сдать вчера
+        //Стоимость: Бесплатно
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public StreamReader file;
+        public int count = 0;
+        public string nameFile = "";
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "GCode|*.gcode";
+            openFileDialog1.Title = "Select a GCode File";
+
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                dataGridView1.Visible = true;
+                clearTable();
+                button1.Visible = true;
+                nameFile = openFileDialog1.SafeFileName;
+                file = new StreamReader(openFileDialog1.FileName);
+                Main();
+            }
+        }
+
+        private void создатьGCodeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            clearTable();
+            button1.Visible = true;
+            dataGridView1.Visible = true;
+        }
+
+        private void Main()
+        {
+            DataGridViewCell main = new DataGridViewCheckBoxCell(); 
+            DataGridViewCell X = new DataGridViewCheckBoxCell(); 
+            DataGridViewCell Y = new DataGridViewCheckBoxCell(); 
+            DataGridViewCell Z = new DataGridViewCheckBoxCell();
+            DataGridViewCell E = new DataGridViewCheckBoxCell(); 
+            DataGridViewCell P = new DataGridViewCheckBoxCell(); 
+            DataGridViewCell S = new DataGridViewCheckBoxCell(); 
+            DataGridViewCell F = new DataGridViewCheckBoxCell(); 
+            DataGridViewCell last = new DataGridViewCheckBoxCell();
+           // DataGridViewRow row = new DataGridViewRow();
+            
+
+            //Добавить комментарии установки температуры, и размеры координат
+            count = 0;
+            string line;
+            short rows = 0;
+            short N = 0;
+            int len = 0;
+            while(true)
+            {
+                last.Value = "";
+                F.Value = "";
+                S.Value = "";
+                P.Value = "";
+                E.Value = "";
+                Z.Value = "";
+                Y.Value = "";
+                X.Value = "";
+                main.Value = "";
+
+                line = file.ReadLine();
+                if(line == null)
+                {
+                    break;
+                }
+                len = line.Length;
+
+                for (int i = 0; i != len; i++)
+                {
+                    switch (line[i])
+                    {
+                        case 'G': last = main; main.Value +="G" + line[i + 1].ToString(); i++; break;
+                        case 'M': last = main; main.Value +="M" + line[i + 1].ToString(); i++; break;
+                        case 'X': last = X; X.Value += line[i + 1].ToString(); i++; break;
+                        case 'Y': last = Y; Y.Value += line[i + 1].ToString(); i++; break;
+                        case 'Z': last = Z; Z.Value += line[i + 1].ToString(); i++; break;
+                        case 'E': last = E; E.Value += line[i + 1].ToString(); i++; break;
+                        case 'P': last = P; P.Value += line[i + 1].ToString(); i++; break;
+                        case 'S': last = S; S.Value += line[i + 1].ToString(); i++; break;
+                        case 'F': last = F; F.Value += line[i + 1].ToString(); i++; break;
+                        case '0': last.Value += line[i].ToString(); break;
+                        case '1': last.Value += line[i].ToString(); break;
+                        case '2': last.Value += line[i].ToString(); break;
+                        case '3': last.Value += line[i].ToString(); break;
+                        case '4': last.Value += line[i].ToString(); break;
+                        case '5': last.Value += line[i].ToString(); break;
+                        case '6': last.Value += line[i].ToString(); break;
+                        case '7': last.Value += line[i].ToString(); break;
+                        case '8': last.Value += line[i].ToString(); break;
+                        case '9': last.Value += line[i].ToString(); break;
+                        case ' ':
+                            
+                            break;
+                        case '.': last.Value += line[i].ToString(); break;
+                    }
+                    
+                    //dataGridView1.Rows[count].Cells[rows].Value += n.ToString();
+                } //end for
+                string[] row = { main.Value.ToString(), X.Value.ToString(), Y.Value.ToString(), Z.Value.ToString(), E.Value.ToString(), P.Value.ToString(), S.Value.ToString(), F.Value.ToString() };
+                dataGridView1.Rows.Add(row);
+                // dataGridView1.Rows.Insert(count);
+                count++;
+                
+            } // end while
+        }
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            date();
+            string filename = nameFile;
+            System.IO.File.WriteAllText(filename, label1.Text);
+            MessageBox.Show("Файл сохранен");
+        }
+
+        private void сохранитьКакToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            date();
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "GCode|*.gcode|All files(*.*)|*.*";
+            saveFileDialog1.Title = "Save an GCode file";
+            saveFileDialog1.FileName = nameFile;
+            saveFileDialog1.DefaultExt = ".gcode";
+            saveFileDialog1.ShowDialog();
+            string filename = saveFileDialog1.FileName;
+            System.IO.File.WriteAllText(filename, label1.Text);
+            MessageBox.Show("Файл сохранен");
+        }
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Add();
+        }
+
+        private void clearTable()
+        {
+            dataGridView1.Rows.Clear();
+            //for (int i = 0; i != )
+        }
+
+        private void date()
+        {
+            label1.Text = "";
+            string[] results = new string[dataGridView1.RowCount];
+            for(int i = 0; i != dataGridView1.RowCount; i++)
+            {
+                results[i] = dataGridView1.Rows[i].Cells[0].Value.ToString(); //G
+                for(int j = 1; j != dataGridView1.ColumnCount; j++)
+                {
+                    string value = dataGridView1.Rows[i].Cells[j].Value.ToString();
+                    if (value != "")
+                    {
+                        results[i] += " ";
+                        results[i] += dataGridView1.Columns[j].Name.ToString();
+                        results[i] += value;
+                    }
+                    
+                }
+                results[i] += "\n";
+                label1.Text += results[i];
+            }
+        }
+    }
+}
